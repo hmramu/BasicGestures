8e27e105c26ca64e052aea26ee2eb7999778775b
Ramu H M <158248964+hmramu@users.noreply.github.com>
2025-04-08 12:06:00 +0530

    Update LongPress_1.java

--- src/test/java/Mobile/BasicGestures/LongPress_1.java
+++ src/test/java/Mobile/BasicGestures/LongPress_1.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 
-public class LongPress extends BaseTest{
+public class LongPress_1 extends BaseTest{
 	
 	@Test
 	public void LongPressGesture() throws InterruptedException 	{
